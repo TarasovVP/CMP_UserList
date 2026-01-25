1ce8e769e8d20d896cfa66ad2d17de9c1e29c2cf
TarasovVP <vptarasov1998@gmail.com>
2026-01-25 08:22:37 +0200

    Update versions

--- shared/src/commonMain/kotlin/com/tarasovvp/cmpuserlist/di/Koin.kt
+++ shared/src/commonMain/kotlin/com/tarasovvp/cmpuserlist/di/Koin.kt
@@ -6,7 +6,6 @@ import org.koin.core.KoinApplication
 import org.koin.core.context.startKoin
 import org.koin.core.module.Module
 
-
 fun initKoin(config: (KoinApplication) -> Unit = {}) {
     startKoin {
         config(this)
